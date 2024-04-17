c4d52d0cefd5538a1e097e033c056250cfb84afc
MinhBang1000 <bradonleminhbang1@gmail.com>
2024-04-17 08:22:36 +0700

    First commint

---
+++ src/test/java/ctu/cit/se/post_service/PostServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ctu.cit.se.post_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
